9b3c206c03fe4e5886b5d4a08b5d6d141f61ac41
brunoalecio <bruno.edualves4@gmail.com>
2025-03-21 16:04:30 -0300

    Iniciando projeto

---
+++ CrudDapperVideo/Services/IUsuarioInterface.cs
@@ -0,0 +1,10 @@
+﻿using CrudDapperVideo.Dto;
+using CrudDapperVideo.Models;
+
+namespace CrudDapperVideo.Services
+{
+    public interface IUsuarioInterface
+    {
+        public Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios();
+    }
+}
---
+++ CrudDapperVideo/Services/UsuarioService.cs
@@ -0,0 +1,20 @@
+﻿using CrudDapperVideo.Dto;
+using CrudDapperVideo.Models;
+
+namespace CrudDapperVideo.Services
+{
+    public class UsuarioService : IUsuarioInterface
+    {
+        private readonly IConfiguration _configuration;
+        public UsuarioService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //Buscar todos usuários
+        public Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
